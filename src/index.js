81839b2c4bfe00fee92d4deaaa52c97ad2bc6bc8
John Beene <wb.john@gmail.com>
2022-07-21 17:58:47 +0700

    add calendar to result

--- src/index.js
+++ src/index.js
@@ -139,9 +139,9 @@ Apify.main(async () => {
             return amenity.id;
           });
 
-        for (const entry of Object.entries(listing_amenities)) {
-          log.info(JSON.stringify(entry));
-        }
+        // for (const entry of Object.entries(listing_amenities)) {
+        //   log.info(JSON.stringify(entry));
+        // }
 
         // log.info("amenities");
         // log.info(JSON.stringify(available_amenities));
